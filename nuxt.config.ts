8ba304e06132afc6537ccc1750eb2d341121abd1
Chris Harden <chardensoft@gmail.com>
2023-09-25 20:42:16 -0400

    turn off server side rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   modules: ['nuxt-headlessui', ['@pinia/nuxt', {autoImports: ['defnieStore', 'acceptHMRUpdate']}]],
   css: ['@/assets/main.css'],

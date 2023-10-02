3cee18a7b00d90c6bd4a7d95d3dcbfcb5e96df53
Chris Harden <chardensoft@gmail.com>
2023-10-02 13:11:40 -0400

    ok now ready to go live

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,6 @@ export default defineNuxtConfig({
     dirs: ['stores']
   },
   routeRules: {
-    '/': { redirect: '/decision'},
     '/cbb': { redirect: '/decision' },
     '/nfldecision': { redirect: '/decision' },
     '/nflrtm': { redirect: '/decision' },

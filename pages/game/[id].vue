333709dee2b3d10249db6397a53421787aac8c1e
Chris Harden <chardensoft@gmail.com>
2023-10-05 11:41:51 -0400

    fixed hank's bug

--- pages/game/[id].vue
+++ pages/game/[id].vue
@@ -354,7 +354,7 @@ const expCard = async (front, i) => {
         const shareData = {
           title: '4th Down Analysis',
           text: 'Check out ESPN\'s analysis of this 4th Down decision on espnanalytics.com/decision',
-          url: 'espnanalytics.com/decision', 
+          url: 'https://espnanalytics.com/decision', 
           files: filesArray,
           dialogTitle: 'Share 4th Down Analysis'
         }

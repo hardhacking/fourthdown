6475edf1ed5fede139e7a8a77a00d8faf4ef311f
Chris Harden <chardensoft@gmail.com>
2023-10-24 15:38:04 -0400

    event -> stoplight

--- pages/stoplight.vue
+++ pages/stoplight.vue
@@ -18,7 +18,7 @@
             </dl>
           </div>
           <div class="pb-6 pt-1">
-            <NuxtLink type="button" :to="'/event/' + index"
+            <NuxtLink type="button" :to="'/stoplight/' + index"
               class="inline-flex items-center gap-x-2 rounded-md bg-espncyan-200 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-espncyan-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-espncyan-200">
               4th Down Stoplight
               <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mt-2 -mx-4" viewBox="0 0 32 40" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"><path d="M22.625,4.741c0,-0.859 -0.341,-1.684 -0.949,-2.292c-0.608,-0.608 -1.433,-0.949 -2.292,-0.949c-2.018,0 -4.75,0 -6.768,0c-0.859,0 -1.684,0.341 -2.292,0.949c-0.608,0.608 -0.949,1.433 -0.949,2.292c0,5.358 0,17.16 0,22.518c-0,0.859 0.341,1.684 0.949,2.292c0.608,0.608 1.433,0.949 2.292,0.949c2.018,-0 4.75,-0 6.768,-0c0.859,-0 1.684,-0.341 2.292,-0.949c0.608,-0.608 0.949,-1.433 0.949,-2.292l-0,-22.518Zm-6.625,16.009c-1.966,-0 -3.562,1.596 -3.562,3.562c-0,1.967 1.596,3.563 3.562,3.563c1.966,-0 3.562,-1.596 3.562,-3.563c0,-1.966 -1.596,-3.562 -3.562,-3.562Zm0,-8.312c-1.966,-0 -3.562,1.596 -3.562,3.562c-0,1.966 1.596,3.562 3.562,3.562c1.966,0 3.562,-1.596 3.562,-3.562c0,-1.966 -1.596,-3.562 -3.562,-3.562Zm0,-8.313c-1.966,0 -3.562,1.596 -3.562,3.563c-0,1.966 1.596,3.562 3.562,3.562c1.966,0 3.562,-1.596 3.562,-3.562c0,-1.967 -1.596,-3.563 -3.562,-3.563Z"/></svg>

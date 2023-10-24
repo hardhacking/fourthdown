1994befb67707fe524778781300d7cd56ffe508b
Chris Harden <chardensoft@gmail.com>
2023-10-24 19:12:04 -0400

    ok more info

--- pages/event/[id].vue
+++ pages/event/[id].vue
@@ -13,7 +13,8 @@
         <div class="mycontainer w-full"> 
             <div class="stoplight-cont">
                 <div id = "outer" class="play-info-container">
-                    <h2 class="text-espngray-900 text-xl font-medium" id="title">{{ colArray.title }}</h2>
+                    <h2 class="text-espngray-900 text-xl font-medium" id="title">{{ colArray.title1 }}</h2>
+                    <h2 class="text-espngray-900 text-xl font-medium" id="title">{{ colArray.title2 }}</h2>
                 </div>
                 <div class="stop-light" id="stop-light">
                     <div class="bulb bg-stoplight-red">
@@ -61,37 +62,59 @@ onMounted(async () => {
 
     if (response.playId) {
         if (response.green) {
+          let home = ''
+          let away = ''
           let ydline = ''
+          const gameString = event.value.shortName
+          const regex = /(\w+)\s@\s(\w+)/
+          const result = regex.exec(gameString)
+          if (result !== null) {
+            const [_, team1, team2] = result
+            const teams = [{ team: team1 }, { team: team2 }]
+            home = teams[1].team
+            away = teams[0].team
+          }
           if (response.yardsToEndzone == response.ballSpot) {
             if (response.yardsToEndzone > 50) {
-              ydline = 'HOME ' + (100 - response.yardsToEndzone)
+              ydline = away + ' ' + (100 - response.yardsToEndzone)
             } else {
-              ydline = 'AWAY ' + response.yardsToEndzone
+              ydline = home + ' ' + response.yardsToEndzone
             }
           } else {
             if (response.yardsToEndzone > 50) {
-              ydline = 'AWAY ' + (100 - response.yardsToEndzone)
+              ydline = home + ' ' + (100 - response.yardsToEndzone)
             } else {
-              ydline = 'HOME ' + response.yardsToEndzone
+              ydline = away + ' ' + response.yardsToEndzone
             }
           }
-            colArray.value = {
-                'green': response.green,
-                'yellow': response.yellow,
-                'red': response.red,
-                'title': response.down + ((response.down == "1") ? 'st' : ((response.down == "2") ? 'nd' : ((response.down == "3") ? 'rd' : 'th'))) + ' & ' + response.distance + ': ' + ydline + ' at ?Q'
-            }
+          colArray.value = {
+            'green': response.green,
+            'yellow': response.yellow,
+            'red': response.red,
+            'title1': event.value.shortName,
+            'title2': response.down + ((response.down == "1") ? 'st' : ((response.down == "2") ? 'nd' : ((response.down == "3") ? 'rd' : 'th'))) + ' & ' + response.distance + ' at ' + ydline + ' (' + ((event.value.status.period < 2) ? '1Q)' : ((event.value.status.period < 5) ? event.value.status.period + 'Q)' : event.value.status.period - 4 + 'OT)'))
+          }
 
-            localStorage.setItem('lastPlay_' + id.value, colArray.value);
+          localStorage.setItem('lastPlay_' + id.value, colArray.value);
         } else {
             colArray.value = JSON.parse(localStorage.getItem('lastPlay_' + id.value))
+            if (!colArray.value) {
+              colArray.value = {
+                'green': '',
+                'yellow': '',
+                'red': '',
+                'title1': event.value.shortName,
+                'title2': 'Waiting for Next Drive'
+              }
+            }
         }
     } else {
       colArray.value = {
-          'green': '',
-          'yellow': '',
-          'red': '',
-          'title': response.displayValue
+        'green': '',
+        'yellow': '',
+        'red': '',
+        'title1': event.value.shortName,
+        'title2': 'Upcoming'
       }
     }
     

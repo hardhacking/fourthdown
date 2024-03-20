129b41f3623185d2dae05daa43c195b51c249e9e
Chris Harden <CHRIS.HARDEN@ESPN.COM>
2024-03-20 17:50:04 -0400

    favicon disappeared

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,29 +3,29 @@
 </template>
 
 <script setup>
-// useSeoMeta({
-//     // title: 'ESPN Analytics',
-//     description: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
-//     ogTitle: 'ESPN Analytics',
-//     ogDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
-//     ogImage: 'https://ea-images-hosting.s3.amazonaws.com/black.png',
-//     ogUrl: 'https://espnanalytics.com',
-//     twitterTitle: 'ESPN Analytics',
-//     twitterDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
-//     twitterImage: 'https://ea-images-hosting.s3.amazonaws.com/black.png',
-//     twitterCard: 'summary'
-// })
+useSeoMeta({
+    // title: 'ESPN Analytics',
+    description: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
+    ogTitle: 'ESPN Analytics',
+    ogDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
+    ogImage: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',
+    ogUrl: 'https://espnanalytics.com',
+    twitterTitle: 'ESPN Analytics',
+    twitterDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
+    twitterImage: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',
+    twitterCard: 'summary'
+})
 
-// useHead({
-//  htmlAttrs: {
-//     lang: 'en'
-//  },
-//  link: [
-//     {
-//     rel: 'icon',
-//     type: 'image/ico',
-//     href: '/favicon.ico'
-//     }
-// ]
-// })
+useHead({
+ htmlAttrs: {
+    lang: 'en'
+ },
+ link: [
+    {
+    rel: 'icon',
+    type: 'image/ico',
+    href: '/favicon.ico'
+    }
+]
+})
 </script>

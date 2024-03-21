987e039a86a0ed11c19a18f084d7cb4de3d219bf
Chris Harden <CHRIS.HARDEN@ESPN.COM>
2024-03-20 20:47:49 -0400

    trying other things with rtms

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,20 @@ useSeoMeta({
     ogTitle: 'ESPN Analytics',
     ogDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
     ogImage: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',
-    ogUrl: 'https://espnanalytics.com',
+    ogImageUrl: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',
+    twitterTitle: 'ESPN Analytics',
+    twitterDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
+    twitterImage: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',
+    twitterCard: 'summary'
+})
+
+useServerSeoMeta({
+    // title: 'ESPN Analytics',
+    description: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
+    ogTitle: 'ESPN Analytics',
+    ogDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
+    ogImage: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',
+    ogImageUrl: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',
     twitterTitle: 'ESPN Analytics',
     twitterDescription: 'Models and data visualization from ESPN Analytics! March Madness, receiver tracking metrics and more.',
     twitterImage: 'https://ea-images-hosting.s3.amazonaws.com/black_475.png',

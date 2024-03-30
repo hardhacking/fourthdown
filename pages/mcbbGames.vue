eaa06989879d523f2e581bdcf3feba5b0709cc5d
Chris Harden <CHRIS.HARDEN@ESPN.COM>
2024-03-30 08:38:40 -0400

    men's round switch

--- pages/mcbbGames.vue
+++ pages/mcbbGames.vue
@@ -102,7 +102,7 @@
         // old_game_ids: [],
         test_bool: [],
         animation_bool: true,
-        chosenRound: 3,
+        chosenRound: 4,
         filtered_ids: [],
       }
     },
@@ -113,7 +113,7 @@
       // window.addEventListener('resize', this.setZoomLevel)
   
       this.loadPage();
-      if (this.chosenRound == 3) {
+      if (this.chosenRound == 4) {
         this.timer = setInterval(() => {
           this.updatePageTemp()
         }, 5000)

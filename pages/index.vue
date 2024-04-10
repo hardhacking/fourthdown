2e3a384ed6e6cab08f77dfe7c0c433f17b450bb9
Chris Harden <CHRIS.HARDEN@ESPN.COM>
2024-04-10 19:32:18 -0400

    bettttter

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
           <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#c31e32] to-[#00aeef] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
         </div>
         <div class="py-0 md:my-[-20px] lg:pb-6">
-        <div class="mx-auto px-6 lg:px-8">
+        <div class="mx-auto px-6 lg:px-8 xl:px-16">
           <div class="mx-auto max-w-2xl text-center">
             <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">ESPN Analytics</h1>
             <p class="mt-6 text-lg leading-8 text-gray-600">Models and data visualization from ESPN Analytics! Run a simulation of the NFL Draft, look at the Draft Day Predictor, compare receiver tracking metrics and more.</p>

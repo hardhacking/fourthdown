d5301cd2a4632d02dfd2c28bacca32e9dce8f781
Chris Harden <CHRIS.HARDEN@ESPN.COM>
2024-05-14 12:42:51 -0400

    we dancin

---
+++ pages/schedule.vue
@@ -0,0 +1,91 @@
+<template>
+    <Head>
+        <Title>NFL Schedule Release Analysis - ESPN Analytics</Title>
+        <Meta property="og:title" content="NFL Schedule Release Analysis - ESPN Analytics"/>
+        <Meta property="og:site_name" content="NFL Schedule Release Analysis - ESPN Analytics" />
+        <Meta property="og:url" content="https://www.espnanalytics.com/schedule" />
+        <Meta name="twitter:url" content="https://www.espnanalytics.com/schedule" />
+        <Meta name="twitter:title" content="NFL Schedule Release Analysis - ESPN Analytics"/>
+        <Meta name="title" content="NFL Schedule Release Analysis - ESPN Analytics"/>
+    </Head>
+    <div class="bg-white">
+        <Header></Header>
+        <div class="pt-24 flex flex-wrap justify-around items-center">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/BEST_NET_REST_DIFFERENCE_ALL_YRS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/BEST_NET_REST_DIFFERENCE.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/BEST_TRAVEL_DIFF.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/FEWEST_BACK-TO-BACK_ROAD_TRIPS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/FEWEST_PRIMETIME_GAMES.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/FEWEST_SHORT_ROAD_WEEKS_ALL_YRS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/FEWEST_SHORT_ROAD_WEEKS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/FEWEST_SHORT_WEEKS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/FEWEST_WEEKS_VS_OPPONENT_OFF_BYE.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/FEWEST_WKS_WITH_LESS_REST.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/LEAST_TRAVEL_MILES.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_BACK-TO-BACK_ROAD_TRIPS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_NET_TRAVEL_MILES_ALL_YRS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_PRIMETIME_GAMES.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_SHORT_ROAD_WEEKS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_SHORT_WEEKS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_TOTAL_TRAVEL_MILES_ALL_YRS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_TRAVEL_MILES.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_WEEKS_VS_OPPONENT_OFF_BYE_ALL_YRS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_WEEKS_VS_OPPONENT_OFF_BYE.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/MOST_WKS_WITH_LESS_REST.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/PAIN_INDEX_ALL_YRS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/WORST_NET_REST_DIFFERENCE_ALL_YRS.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/WORST_NET_REST_DIFFERENCE.jpg">
+            </div>
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+                <img src="@/assets/schedule/WORST_TRAVEL_DIFF.jpg">
+            </div>
+        </div>
+    </div>
+</template>

35e652d69b792b2df79a75861a0b96e2cc534c3a
Chris Harden <CHRIS.HARDEN@ESPN.COM>
2024-05-15 06:03:26 -0400

    update

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -11,79 +11,104 @@
     <div class="bg-white">
         <Header></Header>
         <div class="pt-24 flex flex-wrap justify-around items-center">
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px] 
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/BEST_NET_REST_DIFFERENCE_ALL_YRS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px] 
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/BEST_NET_REST_DIFFERENCE.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px] 
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/BEST_TRAVEL_DIFF.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/FEWEST_BACK-TO-BACK_ROAD_TRIPS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/FEWEST_PRIMETIME_GAMES.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/FEWEST_SHORT_ROAD_WEEKS_ALL_YRS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/FEWEST_SHORT_ROAD_WEEKS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/FEWEST_SHORT_WEEKS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/FEWEST_WEEKS_VS_OPPONENT_OFF_BYE.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/FEWEST_WKS_WITH_LESS_REST.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/LEAST_TRAVEL_MILES.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_BACK-TO-BACK_ROAD_TRIPS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_NET_TRAVEL_MILES_ALL_YRS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_PRIMETIME_GAMES.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_SHORT_ROAD_WEEKS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_SHORT_WEEKS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_TOTAL_TRAVEL_MILES_ALL_YRS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_TRAVEL_MILES.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_WEEKS_VS_OPPONENT_OFF_BYE_ALL_YRS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_WEEKS_VS_OPPONENT_OFF_BYE.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/MOST_WKS_WITH_LESS_REST.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/PAIN_INDEX_ALL_YRS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/WORST_NET_REST_DIFFERENCE_ALL_YRS.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/WORST_NET_REST_DIFFERENCE.jpg">
             </div>
-            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]">
+            <div class="w-72 xxs:w-96 xs:w-[450px] sm:w-[600px]
+                        h-96 xxs:h-[475px] xs:h-[575px] sm:h-[750px] flex justify-center items-center">
                 <img src="@/assets/schedule/WORST_TRAVEL_DIFF.jpg">
             </div>
         </div>
